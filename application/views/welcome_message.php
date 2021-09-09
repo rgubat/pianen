94c40d21b08b9148d5d8b5ff59175502d4b2b68f
rgubat <41900782+rgubat@users.noreply.github.com>
2021-09-09 16:42:17 +0800

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -31,6 +31,35 @@
   <link rel="stylesheet" type="text/css" href="../assets/css/topbar.virtual.css">
 </head>
 <body class="theme-red">
+
+
+	<!-- Messenger Chat Plugin Code -->
+	<div id="fb-root"></div>
+
+	<!-- Your Chat Plugin code -->
+	<div id="fb-customer-chat" class="fb-customerchat">
+	</div>
+
+	<script>
+		var chatbox = document.getElementById('fb-customer-chat');
+		chatbox.setAttribute("page_id", "105625714689543");
+		chatbox.setAttribute("attribution", "biz_inbox");
+
+		window.fbAsyncInit = function() {
+			FB.init({
+				xfbml            : true,
+				version          : 'v11.0'
+			});
+		};
+
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
   
   <!-- Back to top button -->
   <div class="btn-back_to_top">
@@ -665,34 +694,6 @@
       </div>
     </div>
   </div> <!-- End footer -->
-
-	<!-- Messenger Chat Plugin Code -->
-	<div id="fb-root"></div>
-
-	<!-- Your Chat Plugin code -->
-	<div id="fb-customer-chat" class="fb-customerchat">
-	</div>
-
-	<script>
-		var chatbox = document.getElementById('fb-customer-chat');
-		chatbox.setAttribute("page_id", "105625714689543");
-		chatbox.setAttribute("attribution", "biz_inbox");
-
-		window.fbAsyncInit = function() {
-			FB.init({
-				xfbml            : true,
-				version          : 'v11.0'
-			});
-		};
-
-		(function(d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-	</script>
   
   
   <script src="../assets/js/jquery-3.5.1.min.js"></script>
