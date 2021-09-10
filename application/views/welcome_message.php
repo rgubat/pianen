244d8b96cc73bbd788ed58ced54c27b8f6a3d5b5
rgubat <41900782+rgubat@users.noreply.github.com>
2021-09-10 12:01:39 +0800

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -736,7 +736,7 @@
 					Swal.fire({
 						position: 'top-end',
 						icon: 'success',
-						title: 'Your work has been saved',
+						title: 'Email has been sent!',
 						showConfirmButton: false,
 						timer: 1500
 					})
